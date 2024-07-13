4c1eb73fbbc15404c72b1de2bc7607963b6561dd
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-13 09:49:51 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div>
-      test
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
