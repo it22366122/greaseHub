5764bca22cf8db06c1ffd1a1f6a1752d5adcd8de
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-13 10:33:50 +0530

    header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../images/logo.jpeg";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-300 p-4 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <div className="flex items-center space-x-4">
+          <a href="/">
+            <img
+              src={logo}
+              alt="GreaseHub Logo"
+              className="h-20 w-20 rounded-full"
+            />
+          </a>
+          <h1 className="font-bold text-xl flex flex-wrap">
+            <span className="text-slate-500">Grease</span>
+            <span className="text-slate-700">Hub</span>
+          </h1>
+        </div>
+
+        <ul className="flex gap-4  text-gray-800">
+            <Link to="/" className="hover:font-bold">Home</Link>
+            <Link to="/join" className="hover:font-bold">Join Us</Link> 
+            <Link to="/about" className="hover:font-bold">About</Link>
+            <Link to="/signin" className="hover:font-bold">Sign In</Link>
+          
+        </ul>
+      </div>
+    </header>
+  );
+}
