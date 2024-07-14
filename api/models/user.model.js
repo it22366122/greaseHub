c1f67ce51a5bfe58218b7f82264a483bbcc0216c
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-14 12:22:31 +0530

    user model

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import { time } from "console";
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+
+    
+},{timeStamps: true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
