08ec9245023c5ad620fb6ec02087b965e380a758
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-16 18:09:37 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { log } from 'console';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
 
 // Connect to MongoDB
 mongoose.connect(process.env.dbURL).then(() => {
-  log('Connected to MongoDB');
-}).catch((err)=> {console.log(error);});
+  console.log('Connected to MongoDB');
+}).catch((err)=> {console.log(err);});
 
 const app = express();
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+
+
+app.use("/api/user", userRouter); 
