1334d23aefa211231a0b07de26f5cbe7f4a0af66
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-18 11:35:17 +0530

    Hashing the password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,27 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+
 export const signup = async (req, res) => {
 
 
   const {username, email, password} = req.body
 
-  const newUser = new User({username,email,password})
+  //hash the password
+  const hashedPassword = await bcrypt.hash(password, 10)
 
-  await newUser.save()
-  res.status(201).json({message: 'User created successfully!'})
+  const newUser = new User({username,email,password:hashedPassword})
+
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully!" });
+    
+  } catch (error) {
+    res.status(500).json( error.message )
+    
+  }
+  
   
 
 };
