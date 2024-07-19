7bd3d26fc56d9b09647b7be9197d4ba2e752bea4
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-19 17:21:15 +0530

    SignIn API route
    api route, jwt token coockie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import exp from "constants";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,3 +19,32 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+
+  const { username, password } = req.body;
+
+
+  try {
+
+    const validUser = await User.findOne({username});
+    if(!validUser){
+      return next(errorHandler("User credentials not Registered", 401));
+
+    }
+    const validPassword =  bcrypt.compareSync(password, validUser.password);
+    if(!validPassword){
+      return next(errorHandler("Invalid Username of Password", 401));
+    }
+
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    res.cookie("token", token, {httpOnly: true}).status(200).json(validUser);
+
+    
+
+}
+catch (error) {
+  next(error);
+}
+
+}
