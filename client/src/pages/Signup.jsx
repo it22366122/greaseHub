04b978a99656ff73ff564620eaf762eff683da02
Dulaksha Siriwardana <it22366122@my.sliit.lk>
2024-07-19 13:57:50 +0530

    Complete signup function
    Added proxy from port 5000 to 3000

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,36 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+
+      const data = await res.json();
+      console.log(data);
+      
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
   return (
     <div className="flex flex-col w-full max-w-md mx-auto p-4 bg-white shadow-lg rounded-lg">
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
         Sign Up
       </h1>
-      <form className="flex flex-col gap-6">
+      <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
         <input
           className="border border-gray-300 p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="text"
           placeholder="Full Name"
           id="fullName"
+          onChange={handleChange}
         />
         <input
           className="border border-gray-300 p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="email"
           placeholder="E-mail"
           id="email"
+          onChange={handleChange}
         />
         <input
           className="border border-gray-300 p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="text"
           placeholder="Username"
           id="username"
+          onChange={handleChange}
         />
         <input
           className="border border-gray-300 p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="password"
           placeholder="Password"
           id="password"
+          onChange={handleChange}
         />
         <div className="flex items-center gap-2">
           <input
@@ -48,17 +85,15 @@ export default function Signup() {
         </div>
         <button
           type="submit"
-          className="bg-gradient-to-r from-red-800  to-stone-900 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500
-          hover:from-stone-900 hover:to-red-800 font-bold transition duration-500"
+          className="bg-gradient-to-r from-red-800 to-stone-900 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:from-stone-900 hover:to-red-800 font-bold transition duration-500"
         >
           Sign Up
         </button>
       </form>
       <div>
-        <p className="font-mono p-6 ">
+        <p className="font-mono p-6">
           Already registered?{" "}
           <Link to="/signin">
-            {" "}
             <span className="font-mono text-blue-600 hover:underline">
               Sign In
             </span>
